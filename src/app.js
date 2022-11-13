0fb0dc817e954f0642001ff153deff419720d810
AiKurogaya <yujikuroda12@hotmail.com>
2022-11-13 20:12:17 -0300

    feat: Implemented 'participants' API

--- src/app.js
+++ src/app.js
@@ -1,22 +1,45 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-
-await mongoClient.connect();
-db = mongoClient.db("uol_api");
+mongoClient.connect(() => {
+    db = mongoClient.db("uol_api");
+});
 
 const app = express();
 app.use(express.json());
 
 /* Participants Routes */
+app.post("/participants", async (req, res) => {
+
+});
+
+app.get("participants", async (req, res) => {
+    try {
+        const participants = await db.collection("participants").find().toArray();
+        res.send(participants);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
 
 /* Messages Routes */
+app.post("/messages", async (req, res) => {
 
-/* Messages Routes */
+});
+
+app.get("messages", async (req, res) => {
+
+});
+
+/* Status Routes */
+app.post("/status", async (req, res) => {
+
+});
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000.");
